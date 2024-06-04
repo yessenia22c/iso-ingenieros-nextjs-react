ffd88917ed22528a41d9602a213873d5d34e3b26
yessenia22c <yesseniavillarte@gmail.com>
2024-06-04 14:47:18 -0400

    Primer commit

---
+++ src/app/components/NavComponent/NavComponent.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+export const NavComponent = () => {
+    
+    return (
+      <>
+      <nav className='bg-red-800'>
+                 
+          <ul className='max-[499px]:hidden  flex p-2 gap-12 lx:w-[1000px] items-center max-[620px]:text-[14px] max-[620px]:gap-10'>
+              <li>
+                  <img className='w-[90px]' src="./ISO-INGENIEROS-LOGO.svg" alt="" />
+              </li>
+              <li>
+                  <a href='/' className='text-white'>Inicio</a>
+              </li>
+              <li>
+                  <a href='#' className='text-white'>Acerca de</a>
+              </li>
+              <li>
+                  <a href='#' className='text-white'>Cursos</a>
+              </li>
+              <li>
+                  <a href='/contactos' className='text-white'>Contactos</a>
+              </li>
+          </ul>
+            
+      </nav>
+      
+          
+      </>
+      
+      
+    )
+  }
+  
---
+++ src/app/contactos/page.js
@@ -0,0 +1,12 @@
+import { NavComponent } from "../components/NavComponent/NavComponent.jsx";
+
+export default function Contactos () {
+  return (
+    <>
+      <div className="fixed z-20 sm:z-20 w-full flex justify-center bg-red-800 p-2" >
+        <NavComponent />
+      </div>
+      
+    </>
+  )
+}
