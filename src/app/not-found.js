6dd2e5547e5f699200727f2cb63282316c1fe3ad
yessenia22c <yesseniavillarte@gmail.com>
2024-06-07 15:04:26 -0400

    refactor: Add NotFound page component and navigation

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+import PrincipalNavComponent from '@/components/NavComponent/PrincipalNavComponent'
+import { NotFoundComponent } from '@/components/NotFount/NotFoundComponent'
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <>
+      <PrincipalNavComponent></PrincipalNavComponent>
+      <NotFoundComponent></NotFoundComponent>
+    </>
+  )
+}
