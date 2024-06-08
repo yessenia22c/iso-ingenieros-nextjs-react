03498cbcc314d2a9e19466423f68a97945c7c203
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-06-07 21:52:05 -0400

    refactor: Update Contactos page layout and add FormContacto component

--- src/app/contactos/page.js
+++ src/app/contactos/page.js
@@ -1,12 +1,30 @@
+import { FormContacto } from "@/components/Forms/FormContacto.jsx";
 import { NavComponent } from "../../components/NavComponent/NavComponent.jsx";
 
 export default function Contactos () {
   return (
     <>
-      <div className="fixed z-20 sm:z-20 w-full flex justify-center bg-red-800 p-2" >
-        <NavComponent />
-      </div>
-      
+      <section className="flex justify-center">
+        <div className="w-[1000px]">
+          <section className=" grid grid-flow-row sm:grid-cols-2  justify-center gap-4">
+            <article>
+            <h1 className="text-4xl font-semibold my-4">Contáctanos </h1>
+            <p className=" ">
+              ¿Tienes alguna pregunta sobre nuestros cursos de normas ISO o
+              necesitas asesoramiento personalizado? Estamos aquí para ayudarte.
+              Nuestro equipo de expertos está disponible para resolver tus
+              dudas, ofrecerte orientación y apoyarte en el proceso de
+              implementación y certificación de las normas ISO 9001, ISO 22000,
+              ISO 45001 e ISO 14001. No dudes en ponerte en contacto con
+              nosotros a través del formulario a continuación, o llámanos
+              directamente. Juntos, podemos llevar tu organización al siguiente
+              nivel de calidad y eficiencia. ¡Esperamos saber de ti pronto!
+            </p>
+            </article>
+            <FormContacto></FormContacto>
+          </section>
+        </div>
+      </section>
     </>
-  )
+  );
 }
