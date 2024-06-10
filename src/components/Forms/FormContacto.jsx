589a5c627fad2cf30167120fbf2824cb188d12c5
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-06-09 21:56:17 -0400

    feat: Add client-side form submission functionality to FormContacto component

--- src/components/Forms/FormContacto.jsx
+++ src/components/Forms/FormContacto.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+'use client'
+
+import axios from "axios";
+import React, { useRef, useState } from "react";
 
 export const FormContacto = () => {
+
+  const [formData, setFormData] = useState({
+    nombre: "",
+    email: "",
+    telefono: "",
+    mensaje: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    //console.log(name, value);
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+  const formRef = useRef(null);
+  const handleSubmit =  async(e) => {
+    e.preventDefault();
+    const res = await axios.post("/api/contactos", formData);
+    formRef.current.reset()
+    //console.log(formData);
+    console.log('MENSAJE ENVIADO', res);
+  };
+
   return (
     <div className=" bg-gradient-to-tr from-red-800 to-[#f5704b] text-white text-[14px] rounded-xl my-2">
-      <form className="m-8 appearance-none">
+      <form onSubmit={handleSubmit} ref={formRef} className="m-8 appearance-none">
         <div className="flex flex-col ">
           <label className="my-2" name="nombre">Nombre</label>
           <input
@@ -12,13 +40,17 @@ export const FormContacto = () => {
              ring-gray-600 border-0 rounded-md p-2 
              focus:ring-2 focus:ring-inset focus:ring-red-400"
             type="text"
-            id="nombre"
+            id="nombre"value={formData.nombre}
             name="nombre"
+
+            onChange={handleChange}
           />
         </div>
         <div className="flex flex-col">
           <label className="my-2" name="email">Correo electrónico</label>
           <input 
+          value={formData.email}
+          onChange={handleChange}
           className=" text-gray-800 appearance-none outline-none
           ring-gray-600 border-0 rounded-md p-2 
           focus:ring-2 focus:ring-inset focus:ring-red-400"
@@ -27,6 +59,8 @@ export const FormContacto = () => {
         <div className="flex flex-col">
           <label className="my-2" name="telefono">Telefono</label>
           <input 
+          value={formData.telefono}
+          onChange={handleChange}
           placeholder="(xxx) xxx-xx-xxx"
             className=" text-gray-800 appearance-none outline-none placeholder:text-gray-300
             ring-gray-600 border-0 rounded-md p-2 
@@ -36,6 +70,8 @@ export const FormContacto = () => {
         <div className="flex flex-col">
           <label className="my-2" name="mensaje">Mensaje</label>
           <textarea 
+          value={formData.mensaje}
+          onChange={handleChange}
           placeholder="Escribe aquí tu mensaje..."
           rows={3}
             className=" text-gray-800 appearance-none outline-none placeholder:text-gray-300
@@ -43,7 +79,7 @@ export const FormContacto = () => {
             focus:ring-2 focus:ring-inset focus:ring-red-400"
           id="mensaje" name="mensaje" />
         </div>
-        <button className="my-2" type="submit">Enviar</button>
+        <button className="my-2" type="submit" >Enviar</button>
       </form>
     </div>
   );
