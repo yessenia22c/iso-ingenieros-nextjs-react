31e96000dcfcd0905ad28f56ecfa06cc209ee376
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-06-09 21:56:24 -0400

    refactor: Update NotFoundComponent layout and content

--- src/components/NotFount/NotFoundComponent.jsx
+++ src/components/NotFount/NotFoundComponent.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 export const NotFoundComponent = () => {
   return (
-    <div>
-        <h1>
-            404 - Page Not Found
-        </h1>
-    </div>
+    <seccion className='w-[1000px]'>
+      <div className='flex flex-col items-center justify-center h-[500px]'>
+        <h1 className='text-6xl font-bold text-red-800'>404</h1>
+        <p className='text-xl'> Lo sentimos, parece que esta página no éxiste.</p>
+
+        <p className='text-xl font-bold'>¿Qué quieres hacer?</p>
+        <ul>
+          <li>
+            <a className=' text-gray-950 hover:text-red-400' href='/'>Ir a la página principal</a>
+          </li>
+          <li>
+            <a className='  text-gray-950 hover:hover:text-red-400' href='/contacto'>Mirar nuestros cursos</a>
+          </li>
+        </ul>
+      </div>
+    </seccion>
   )
 }
 
