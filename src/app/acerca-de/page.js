bd42a41c8c7ef7cd0313ef0e0fd6d5412d696f22
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-06-09 21:55:53 -0400

    refactor: Update AcercaDe page layout and content

--- src/app/acerca-de/page.js
+++ src/app/acerca-de/page.js
@@ -4,8 +4,29 @@ import PrincipalNavComponent from "../../components/NavComponent/PrincipalNavCom
 export default function page()  {
   return (
     <>
-    {/* <PrincipalNavComponent /> */}
-    <div>page</div>
+    <section className="flex justify-center">
+    <div className="w-[1000px] px-4">
+        <div className="grid grid-cols-2 justify-center">
+        <h1 className="text-4xl font-semibold my-4">Acerca de </h1>
+        <p className=" ">
+          Somos una empresa de consultoría especializada en la implementación de
+          sistemas de gestión de calidad, seguridad alimentaria, seguridad y salud
+          en el trabajo, y gestión ambiental. Nuestro equipo de expertos cuenta con
+          más de 20 años de experiencia en el diseño, implementación y certificación
+          de sistemas de gestión basados en las normas ISO 9001, ISO 22000, ISO 45001
+          e ISO 14001. Nos apasiona ayudar a las organizaciones a mejorar su eficiencia,
+          productividad y competitividad a través de la implementación de sistemas de
+          gestión de calidad y seguridad. Nuestros servicios incluyen la consultoría,
+          formación, auditoría y certificación de sistemas de gestión, así como el
+          desarrollo de herramientas y recursos para facilitar la implementación y
+          mantenimiento de los sistemas de gestión. Nuestro objetivo es ser el socio
+          estratégico de las organizaciones en su camino hacia la excelencia y la
+          sostenibilidad. ¡Contáctanos y descubre cómo podemos ayudarte a alcanzar tus
+          objetivos de calidad y seguridad!
+        </p>
+        </div>
+    </div>
+    </section>
     </>
   )
 }
