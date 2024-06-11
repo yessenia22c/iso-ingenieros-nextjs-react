a0e91b7f52fb1cfdbac39fae15ebf0253a50db77
yessenia22c <yesseniavillarte@gmail.com>
2024-06-11 15:18:58 -0400

    refactor: Add client-side form submission functionality to FormContacto component Sin completar

--- src/components/MainComponent/BannerComponent.jsx
+++ src/components/MainComponent/BannerComponent.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { FormContacto } from '../Forms/FormContacto'
 
 export const BannerComponent = () => {
   return (
-    <div className='xl:h-screen bg-gradient-to-tr from-red-400 to-[#000000a1] relative  sm:object-top'>
+    <>
+    <div className='flex absolute object-right'>
+      <FormContacto></FormContacto>
+      </div>
+    <div className='xl:h-screen bg-gradient-to-tr from-red-400 to-[#000000a1] relative  sm:object-top'> 
       <img className='sm:object-top sm:object-scale-down flex w-full xl:h-screen xl:object-cover z-10 mix-blend-multiply' src="./img/Fondo.webp" alt="" />
     </div>
+    </>
     
   )
 }
