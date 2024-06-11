45f5c5fcc9ed43a16154a6a8a801688b0bad1a15
yessenia22c <yesseniavillarte@gmail.com>
2024-06-11 15:12:37 -0400

    refactor: Update button styles in PrincipalNavComponent

--- src/components/NavComponent/PrincipalNavComponent.jsx
+++ src/components/NavComponent/PrincipalNavComponent.jsx
@@ -38,7 +38,7 @@ function PrincipalNavComponent() {
       <NavComponent></NavComponent>
       <ul className='flex w-full justify-end min-[500px]:hidden '>
         <li >
-          <button onClick={ abrirCerrarAside }  className="justify-end text-white font-medium rounded-lg text-sm px-5 py-1" type="button" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
+          <button onClick={ abrirCerrarAside }  className=" bg-red-800 hover:bg-red-700 justify-end text-white font-medium rounded-lg text-sm px-5 py-1" type="button" data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation">
               <span className="sr-only">Open sidebar</span>
               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
