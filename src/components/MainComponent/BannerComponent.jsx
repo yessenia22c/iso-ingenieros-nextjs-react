3ca81674b2cc641a63b6b72e55803c3e9c9bbc55
yessenia22c <yesseniavillarte@gmail.com>
2024-06-12 11:08:34 -0400

    refactor: Update BannerComponent layout and add contact button functionality

--- src/components/MainComponent/BannerComponent.jsx
+++ src/components/MainComponent/BannerComponent.jsx
@@ -1,13 +1,23 @@
+'use client'
 import React from 'react'
 import { FormContacto } from '../Forms/FormContacto'
+import { useRouter } from 'next/navigation'
+
+
 
 export const BannerComponent = () => {
+  const router = useRouter()
+  const onContact = (e) => {
+    e.preventDefault()
+    router.push('/contactos')
+  }
   return (
     <>
-    <div className='flex absolute object-right'>
-      <FormContacto></FormContacto>
+    <div className='flex justify-center items-center xl:h-screen bg-gradient-to-tr from-red-400 to-[#000000a1] relative  sm:object-top'> 
+      <div className='z-[12] flex-col items-center text-center justify-center absolute'>
+        <h1 className='text-center text-white  text-[22px] sm:text-4xl xl:text-6xl font-semibold'>Cursos de formación profesional</h1>
+        <button onClick={onContact} className='my-4 text-sm sm:text-xl'>Contáctanos</button>
       </div>
-    <div className='xl:h-screen bg-gradient-to-tr from-red-400 to-[#000000a1] relative  sm:object-top'> 
       <img className='sm:object-top sm:object-scale-down flex w-full xl:h-screen xl:object-cover z-10 mix-blend-multiply' src="./img/Fondo.webp" alt="" />
     </div>
     </>
