24e47e3bdc3cacdaec1e16bb4509ac0000ece28b
yessenia22c <yesseniavillarte@gmail.com>
2024-06-14 08:54:00 -0400

    refactor: Update NotFoundComponent layout and content

--- src/components/NotFount/NotFoundComponent.jsx
+++ src/components/NotFount/NotFoundComponent.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-
+import Link from 'next/link'
 export const NotFoundComponent = () => {
   return (
     <seccion className='w-[1000px]'>
-      <div className='flex flex-col items-center justify-center h-[500px]'>
+      <div className='flex flex-col items-center justify-center h-[600px]'>
         <h1 className='text-6xl font-bold text-red-800'>404</h1>
-        <p className='text-xl'> Lo sentimos, parece que esta página no éxiste.</p>
+        <p className='text-xl flex flex-col justify-center items-center text-center'>Lo sentimos, parece que esta página no éxiste.</p>
 
         <p className='text-xl font-bold'>¿Qué quieres hacer?</p>
-        <ul>
+        <ul className='flex flex-col justify-center items-center'>
           <li>
-            <a className=' text-gray-950 hover:text-red-400' href='/'>Ir a la página principal</a>
+            <Link className=' text-gray-950 hover:text-red-400' href='/'>Ir a la página principal</Link>
           </li>
           <li>
-            <a className='  text-gray-950 hover:hover:text-red-400' href='/contacto'>Mirar nuestros cursos</a>
+            <Link className='  text-gray-950 hover:hover:text-red-400' href='/contactos'>Contáctanos ahora</Link>
           </li>
         </ul>
       </div>
