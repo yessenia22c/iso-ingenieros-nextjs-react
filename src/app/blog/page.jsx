0f4e8109ea06a19c46b44054abc8dd63aae9a740
yessenia22c <65354520+yessenia22c@users.noreply.github.com>
2024-08-22 21:45:33 -0400

    refactor: Update API route to fetch and display blog posts

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import { Blog } from '@/components/Blog/Blog'
 
-export default function page () {
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3000/api/posts');
+  const posts = await res.json();
+
+  return {
+      props: {
+          posts,
+      },
+  };
+}
+
+export default function page ({posts}) {
+  
   return (
-    <Blog />
+    <div>
+        <h1>Blog</h1>
+        {loading ? (
+            <p>Cargando...</p>
+        ) : (
+            <ul>
+            {posts.map((post) => (
+                <li key={post.id}>{post.title.rendered}</li>
+            ))}
+            </ul>
+        )}
+    </div>
   )
 }
--- src/lib/post.js
+++ src/lib/post.js
@@ -1,25 +1,23 @@
-'use client'
+
 export async function fetchAPI(endpoint) {
-    const apiUrl = process.env.NEXT_PUBLIC_WORDPRESS_API_BACKEND;
-    const res = await fetch(`${apiUrl}/${endpoint}`, {
-    // headers: {
-    //   Authorization: `Bearer ${process.env.WORDPRESS_API_TOKEN}`,
-    // },
-  });
-  console.log(process.env.NEXT_PUBLIC_WORDPRESS_API_URL);
+  const apiUrl = process.env.NEXT_PUBLIC_WORDPRESS_API_BACKEND;
+  console.log(`${apiUrl}/${endpoint}`)
+  const res = await fetch(`${apiUrl}/${endpoint}`);
+  
   if (!res.ok) {
-    throw new Error('Error al hacer la solicitud');
+      throw new Error('Error al hacer la solicitud');
   }
 
   return await res.json();
 }
 
 export async function getAllPosts() {
-    const apiUrl = process.env.NEXT_PUBLIC_WORDPRESS_API_BACKEND;
-    console.log(apiUrl)
-    const res = await fetch(`${apiUrl}/wp/v2/posts`);
-    if (!res.ok) {
-        throw new Error('Error al hacer la solicitud');
-    }
-    return await res.json();
+  console.log(NEXT_PUBLIC_WORDPRESS_API_BACKEND)
+  const apiUrl = process.env.NEXT_PUBLIC_WORDPRESS_API_BACKEND;
+  const res = await fetch(`${apiUrl}/wp/v2/posts`);
+
+  if (!res.ok) {
+    throw new Error('Error al hacer la solicitud');
+}
+  return res.json();
 }
