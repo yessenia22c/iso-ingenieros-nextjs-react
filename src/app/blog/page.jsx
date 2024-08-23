43037ce6431cc02934a01465165eea55ad06728d
yessenia22c <yesseniavillarte@gmail.com>
2024-08-23 15:16:05 -0400

    refactor: Update blog page to fetch and display blog posts

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,31 +1,67 @@
-import React from 'react'
-import { Blog } from '@/components/Blog/Blog'
+import { Loading } from '@/components/Loading';
+import axios from 'axios';
+import { ListaBlog } from '@/components/Blog/ListaBlog';
+import DOMPurify from 'isomorphic-dompurify';
 
-export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3000/api/posts');
-  const posts = await res.json();
+import Link from 'next/link'
 
-  return {
-      props: {
-          posts,
-      },
-  };
-}
+async function getBlogs() {
 
-export default function page ({posts}) {
+  const apiUrl = process.env.DOMINIO_FRONTEND;
   
+  const res = await axios.get(`${apiUrl}/api/blog`).then((response) => {
+    //console.log(response.data)
+    return response.data;
+  }).catch(error => {
+    console.error('Error al obtener los posts:', error);
+    return null;
+  });
+
+  return res;
+  
+}
+export const dynamic = 'force-dynamic'
+
+async function BlogsPage () {
+
+  const posts = await getBlogs()
+  //console.log(posts)
+  
+
+  //console.log(posts)
   return (
-    <div>
-        <h1>Blog</h1>
-        {loading ? (
-            <p>Cargando...</p>
-        ) : (
-            <ul>
-            {posts.map((post) => (
-                <li key={post.id}>{post.title.rendered}</li>
-            ))}
-            </ul>
-        )}
-    </div>
+    <section className="flex justify-center">
+      <div className="w-[1000px] px-4">
+        
+          <h1 className='text-xl font-bold py-5'>Blog</h1>
+          {posts ? (
+            <div>
+            { posts.map((post) => {
+              
+                const parrafo = DOMPurify.sanitize(post.excerpt.rendered, { SAFE_FOR_TEMPLATES: { html: true } });
+                return (
+                  <article className='border rounded-md p-5 my-3' key={post.id}>
+                  <h2 className='text-xl font-semibold'>
+                    <Link href={`blog/${post.slug}`}> {post.title.rendered}</Link>
+                  </h2>
+                  {<div dangerouslySetInnerHTML={{ __html: parrafo }} />}
+                  
+                  </article>
+                )
+              })
+            }
+            </div>
+              
+          ) : (
+            <Loading verificar={true}></Loading>
+          )}
+      </div>
+    </section>
+
+    // <div>
+    //   <ListaBlog></ListaBlog>
+    // </div>
   )
 }
+
+export default BlogsPage
